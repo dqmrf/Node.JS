701ccaf8366a19227d13e60ee1fbe30b17822b41
Misha Pelih <jscript@meta.ua>
2016-11-22 17:42:42 +0200

    +1

--- streams/server2.js
+++ streams/server2.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 
 var readStream = fs.createReadStream('index.md');
 var data = '';
+var chunk;
 
-readStream.on('data', function(chunk){
-    data += chunk;
+readStream.on('readable', function(){
+    while((chunk = readStream.read()) != null) {
+        data += chunk;
+    }
 });
 
 readStream.on('end', function(){
