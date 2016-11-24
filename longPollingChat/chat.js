79ba32c697039f952ac2d88b4c66532d6caf70bc
misha-pelykh <jscript@meta.ua>
2016-11-24 22:35:14 +0200

    long-polling chat finished

--- longPollingChat/chat.js
+++ longPollingChat/chat.js
@@ -1,14 +1,20 @@
 var clients = [];
 
 exports.subscribe = function(request, response) {
-    console.log('![chat]: subscribe');
-    clients.push(response);   
+    console.log('[chat] subscribe');
+
+    clients.push(response);
+
+    response.on('close', () => {
+        console.log('[chat] close');
+        clients.splice(clients.indexOf(response), 1);
+    });
 }
 
 exports.publish = function(message) {
-    console.log("![chat]: publish '%s'", message);
+    console.log("[chat] publish '%s'", message);
 
-    clients.forEach(function(res){
+    clients.forEach(function(res) {
         res.end(message);
     });
 
--- longPollingChat/server.js
+++ longPollingChat/server.js
@@ -3,40 +3,67 @@ const fs = require('fs');
 const chat = require('./chat');
 
 // Entry Point.
-http.createServer(function(request, respond) {
+http.createServer(function(request, response) {
     switch (request.url) {
         case '/':
-            sendFile('index.html', respond);
+            sendFile('index.html', response);
             break;
 
         case '/subscribe':
-            chat.subscribe(request, respond);
+            chat.subscribe(request, response);
             break;
 
         case '/publish':
-            chat.publish('...');
+            var data = '';
+
+            request
+                .on('readable', () => {
+                    var chunk;
+
+                    if (!(chunk = request.read())) return false;
+
+                    if (data.length > 1e2) {
+                        response.statusCode = 413;
+                        response.end("Your message is too big :-)");
+                        request.emit("end");
+                    }
+
+                    data += chunk;
+                })
+                .on('end', () => {
+                    try {
+                        data = JSON.parse(data);
+                    } catch (e) {
+                        response.statusCode = 400;
+                        response.end("Bad request");
+                        return;
+                    }
+                    chat.publish(data.message);
+                    response.end("ok");
+                });
             break;
 
         default:
-            respond.statusCode = 404;
-            respond.end('Page not found {switch}');
+            response.statusCode = 404;
+            response.end('Page not found {switch}');
+            break;
     }
 }).listen(3000);
 
 
 // Functions.
 function sendFile(filePath, response) {
-    var file = fs.createReadStream(filePath);
+    var readStream = fs.createReadStream(filePath);
 
-    file.pipe(response);
+    readStream.pipe(response);
 
-    file.on('error', (err) => {
+    readStream.on('error', (err) => {
         response.statusCode = 500;
         response.end('Server Error!');
         console.error(err);
     });
 
     response.on('close', () => {
-        file.destroy();
+        readStream.destroy();
     });
 }
